f8ee676fc132a0d081bd5fe2673c5d6833d38aaf
Paweł Fajfer <p.fajfer@piwik.pro>
2026-06-30 13:36:36 +0200

    Bump nuxt from 3.12.4 to 4.4.8 in /example

--- example/nuxt.config.ts
+++ example/nuxt.config.ts
@@ -2,7 +2,7 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   css: ["~/assets/css/main.css"],
   modules: [
-    "@nuxtjs/eslint-module",
+    "@nuxt/eslint",
     [
       "@piwikpro/nuxt-piwik-pro",
       {
